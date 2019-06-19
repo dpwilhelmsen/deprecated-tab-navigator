4cf6163e9b4da8b1b61de8cb2a4dd823dc4b6c0d
Daniel Wilhelmsen <dpwilhelmsen@users.noreply.github.com>
2019-06-19 10:44:20 -0700

    Update index export to proper ES6 export

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import TabView from './views/TabView';
 import TabBarBottom from './views/TabBarBottom';
 import TabBarTop from './views/TabBarTop';
 
-export createTabNavigator;
-export TabView;
-export TabBarBottom;
-export TabBarTop;
+export {
+  createTabNavigator,
+  TabView,
+  TabBarBottom,
+  TabBarTop,
+};
